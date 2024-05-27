52c77476dd3dec2a6b15bc3f769035b46dcaece4
princy-singh-rajpoot <princyvandanasingh@gmail.com>
2024-05-27 23:40:51 +0530

    commmiting imp views and urls

--- emp_project/emp_app/urls.py
+++ emp_project/emp_app/urls.py
@@ -1,7 +1,10 @@
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index)
+    path('',views.index, name='index'),
+    path('view_emp',views.all_emp, name='all_emp'),
+    path('add_emp',views.add_emp, name='add_emp'),
+    path('filter_emp',views.filter_emp, name='filter_emp'),
+    path('remove_emp',views.remove_emp, name='remove_emp'),
 ]
--- emp_project/emp_app/views.py
+++ emp_project/emp_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Employee,Department,Role
 
 # Create your views here.
+def index(request):
+    return render (request,'index.html')
+
+def all_emp(request):
+    emps = Employee.objects.all()
+    context = {
+        'emps' : emps
+    }
+    return render(request,'view_emp.html',context)
+
+def add_emp(request):
+    return render(request,'add_emp.html')
+
+def remove_emp(request):
+    return render(request,'remove_emp.html')
+
+def filter_emp(request):
+    return render(request,'filter_emp.html')
